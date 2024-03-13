bc21c0a23749e095f6a4f2ddb234006a5137591e
onlybuzy <josezuncaz@gmail.com>
2024-03-13 12:12:16 -0500

    100

--- SP_1.py
+++ SP_1.py
@@ -6,7 +6,7 @@ from gpiozero_extended import Motor, PID
 # Setting general parameters
 tstop = 5  # Execution duration (s)
 tsample = 0.01  # Sampling period (s)
-wsp = 20  # Motor speed set point (rad/s)
+wsp = 100  # Motor speed set point (rad/s)
 tau = 0.1  # Speed low-pass filter response time (s)
 
 # Creating PID controller object
